f6ff035fb9ecfacc136f9209d0e63258dbb1b5f6
sanjeevWeb <kumarsanjeevdutta02@gmail.com>
2024-02-11 17:03:42 +0530

    created user schema

--- backend/server.js
+++ backend/server.js
@@ -12,14 +12,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('5baa2528563f16379fc8a610')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('5baa2528563f16379fc8a610')
+    .then(user => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -27,15 +27,27 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    'mongodb://localhost:27017'
-  )
-  .then(result => {
-    app.listen(3000);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+    .connect(
+        'mongodb://localhost:27017'
+    )
+    .then(result => {
+        User.findOne().then(user => {
+            if (!user) {
+                const user = new User({
+                    name: 'Max',
+                    email: 'max@test.com',
+                    cart: {
+                        items: []
+                    }
+                });
+                user.save();
+            }
+        });
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
 // mongoConnect(client => {
 //     console.log(client)
