d94e32105dd8c36566023ab0431a846f7610f62c
sanjeevWeb <kumarsanjeevdutta02@gmail.com>
2024-02-11 16:46:47 +0530

    working with mongoose ODM in node

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const mongoConnect = require('./db/database.js')
+const express = require('express');
+const mongoose = require('mongoose');
 
-const app = express()
+// const User = require('./models/user');
+
+const app = express();
+
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -9,22 +12,35 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-    User.findById('5baa2528563f16379fc8a610')
-        .then(user => {
-            req.user = new User(user.name, user.email, user.cart, user._id);
-            next();
-        })
-        .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('5baa2528563f16379fc8a610')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-mongoConnect(client => {
-    console.log(client)
-    app.listen(3000, () => {
-        console.log('app listening at 3000')
-    })
+app.use(errorController.get404);
 
-})
+mongoose
+  .connect(
+    'mongodb://localhost:27017'
+  )
+  .then(result => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+// mongoConnect(client => {
+//     console.log(client)
+//     app.listen(3000, () => {
+//         console.log('app listening at 3000')
+//     })
+
+// })
